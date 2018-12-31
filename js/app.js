9817318c577df0c2ce0baac77115927178f6fae0
Jimmy D <jd9192016@gmail.com>
2018-12-31 17:19:28 -0500

    2nd update
    Alerts css adn JavaScript added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 var chartAlert = document.getElementById("chartAlert");
+var bellAlert = document.getElementById("bell");
+var headerAlerts= document.getElementById("header_alerts");
 document.getElementById("x").addEventListener("click", function() {
     chartAlert.style.display="none";
 }); 
+
+bellAlert.addEventListener("click", function() {
+    if(headerAlerts.style.display=="none"){
+        headerAlerts.style.display="inline-block";
+     
+        bellDot.style.display="none";
+       } else{
+           headerAlerts.style.display="none";
+           bellDot.style.display="inline-block";
+       }
+}); 
+
+
